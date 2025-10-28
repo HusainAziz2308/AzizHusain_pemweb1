e10dca5034b05c2f81dbd99f67f12344b4418175
Husain Aziz <husainazizalrosyid@gmail.com>
2025-10-28 03:11:27 +0000

    html in php

--- Pertemuan 7 - PHP/php/index.php
+++ Pertemuan 7 - PHP/php/index.php
@@ -1,22 +1,35 @@
-<?php
-$Pemasukan = 50000000;
-$HutangA = 16000000;
-$BungaHutangA = 0.05;
-$HutangB = 9500000;
-$BungaHutangB = 0.045;
+<!DOCTYPE html>
+<html lang="en">
 
-// Menghitung total bunga hutang
-$TotalBungaHutangA = $HutangA * $BungaHutangA;
-$TotalBungaHutangB = $HutangB * $BungaHutangB;
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-// Menghitung total
-$TotalHutang = $HutangA + $HutangB;
-$TotalBungaHutang = $TotalBungaHutangA + $TotalBungaHutangB;
-$TotalPembayaran = $TotalHutang + $TotalBungaHutang;
-$SisaPemasukan = $Pemasukan - $TotalPembayaran;
+<body>
+    <?php
+    $Pemasukan = 50000000;
+    $HutangA = 16000000;
+    $BungaHutangA = 0.05;
+    $HutangB = 9500000;
+    $BungaHutangB = 0.045;
 
-echo "Total Hutang: Rp " . number_format($TotalHutang, 0, ',', '.') . "<br>";
-echo "Total Bunga Hutang: Rp " . number_format($TotalBungaHutang, 0, ',', '.') . "<br>";
-echo "Total Pembayaran: Rp " . number_format($TotalPembayaran, 0, ',', '.') . "<br>";
-echo "Sisa Pemasukan Setelah Pembayaran Hutang: Rp " . number_format($SisaPemasukan, 0, ',', '.') . "<br>";
-?>
+    // Menghitung total bunga hutang
+    $TotalBungaHutangA = $HutangA * $BungaHutangA;
+    $TotalBungaHutangB = $HutangB * $BungaHutangB;
+
+    // Menghitung total
+    $TotalHutang = $HutangA + $HutangB;
+    $TotalBungaHutang = $TotalBungaHutangA + $TotalBungaHutangB;
+    $TotalPembayaran = $TotalHutang + $TotalBungaHutang;
+    $SisaPemasukan = $Pemasukan - $TotalPembayaran;
+
+    echo "Total Hutang: Rp " . number_format($TotalHutang, 0, ',', '.') . "<br>";
+    echo "Total Bunga Hutang: Rp " . number_format($TotalBungaHutang, 0, ',', '.') . "<br>";
+    echo "Total Pembayaran: Rp " . number_format($TotalPembayaran, 0, ',', '.') . "<br>";
+    echo "Sisa Pemasukan Setelah Pembayaran Hutang: Rp " . number_format($SisaPemasukan, 0, ',', '.') . "<br>";
+    ?>
+</body>
+
+</html>
