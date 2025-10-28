c029a58bb4c051975f252d02cafdf3c33a969944
Husain Aziz <husainazizalrosyid@gmail.com>
2025-10-28 02:23:46 +0000

    addd index php

---
+++ Pertemuan 7 - PHP/index.php
@@ -0,0 +1,13 @@
+<?php
+$Pemasukan = 50000000;
+$HutangA = 16000000;
+$BungaHutangA = 0.05;
+$HutangB = 9500000;
+$BungaHutangB = 0.045;
+
+// Menghitung total bunga hutang
+$TotalBungaHutangA = $HutangA * $BungaHutangA;
+$TotalBungaHutangB = $HutangB * $BungaHutangB;
+$TotalBungaHutang = $TotalBungaHutangA + $TotalBunga;
+
+?>
