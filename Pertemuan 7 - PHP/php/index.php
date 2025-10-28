f629008a4e335795f26d30e36733d63e66d51357
Husain Aziz <husainazizalrosyid@gmail.com>
2025-10-28 02:31:30 +0000

    echo php

---
+++ Pertemuan 7 - PHP/php/index.php
@@ -0,0 +1,22 @@
+<?php
+$Pemasukan = 50000000;
+$HutangA = 16000000;
+$BungaHutangA = 0.05;
+$HutangB = 9500000;
+$BungaHutangB = 0.045;
+
+// Menghitung total bunga hutang
+$TotalBungaHutangA = $HutangA * $BungaHutangA;
+$TotalBungaHutangB = $HutangB * $BungaHutangB;
+
+// Menghitung total
+$TotalHutang = $HutangA + $HutangB;
+$TotalBungaHutang = $TotalBungaHutangA + $TotalBungaHutangB;
+$TotalPembayaran = $TotalHutang + $TotalBungaHutang;
+$SisaPemasukan = $Pemasukan - $TotalPembayaran;
+
+echo "Total Hutang: Rp " . number_format($TotalHutang, 0, ',', '.') . "<br>";
+echo "Total Bunga Hutang: Rp " . number_format($TotalBungaHutang        , 0, ',', '.') . "<br>";
+echo "Total Pembayaran: Rp " . number_format($TotalPembayaran, 0, ',', '.') . "<br>";
+echo "Sisa Pemasukan Setelah Pembayaran Hutang: Rp " . number_format($SisaPemasukan, 0, ',', '.') . "<br>"; 
+?>
